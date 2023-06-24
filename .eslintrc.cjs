030d1ffcd4027676d9b99276f8e34ed9685dd775
TheGuzman <guzmanmedrano.diego@gmail.com>
2023-06-24 18:00:27 +0200

    fix(eslint): revert  override to set rule no-misused-promises

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,23 +17,5 @@ module.exports = {
     'object-curly-spacing': ['Error', 'always'],
     'new-cap': ['error', { capIsNewExceptions: ['express.Router'] }],
     'operator-linebreak': ['error', 'after'],
-    '@typescript-eslint/no-misused-promises': [
-      'error',
-      {
-        checksVoidReturn: false,
-      },
-    ],
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
-    },
-  ],
 };
