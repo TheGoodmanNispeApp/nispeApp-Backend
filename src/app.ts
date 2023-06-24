d164e2bbe5979e90c6ccd6be98e861486dd13a20
TheGuzman <guzmanmedrano.diego@gmail.com>
2023-06-24 17:29:42 +0200

    feat(api-router): add main api router and add to app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import apiRouter from './api/api-router.js';
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.get('/', (_req, res) => {
 app.use(cors());
 app.use(express.json());
 app.disable('x-powered-by');
+
+app.use('/api/v1', apiRouter);
 
 export default app;
